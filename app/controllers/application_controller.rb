0cf0e0f67766392a001dac0e53bc869045dc3861
yuhei-tsuchiya <yuhei.1711.555@gmail.com>
2019-09-04 12:40:52 +0900

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?　　#デバイスに関する画面に行ったとき,パラメーターを取得
+
+  protected #privateと同じ
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
