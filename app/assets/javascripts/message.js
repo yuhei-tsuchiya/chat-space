df74e42d8d7c1c4e5aa3034c0e00e71a6fc8f3a3
yuhei-tsuchiya <yuhei.1711.555@gmail.com>
2019-09-09 11:52:01 +0900

    lll

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,53 @@
+$(function(){
+  function buildHTML(message){
+
+    var image = message.image? `<img class="messages__text-box__image" src="${message.image}"></img>`: ``
+    var html =  `<div class="messages__top-box">
+                  <div class="messages__top-box__mini-box">
+                    <div class="messages__top-box__mini-box__name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="messages__text-box">
+                    <p class="messages__text-box__text">
+                      ${message.content}
+                    </p>
+                  </div>
+                  <div class="messages__text-box">
+                    ${image}
+                  </div>
+                </div> `
+
+  return html
+  }
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $('.submit-btn').removeAttr('data-disable-with');
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.messages').append(html)
+      $('.message_content.input-box__text').val('');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'slow');
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
