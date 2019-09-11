6bfe21a54655e23eba94c5faf1f882161ded4bd6
yuhei-tsuchiya <yuhei.1711.555@gmail.com>
2019-09-11 14:46:01 +0900

    automatic_update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
 
     var image = message.image? `<img class="messages__text-box__image" src="${message.image}"></img>`: ``
-    var html =  `<div class="messages__top-box">
+    var html =  `<div class="messages__top-box" data-id= "${message.id}">
                   <div class="messages__top-box__mini-box">
                     <div class="messages__top-box__mini-box__name">
                       ${message.user_name}
@@ -51,4 +51,34 @@ $(function(){
       alert('error');
     })
   })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.messages__top-box:last').data("id")
+        console.log($('.messages__top-box:last').data("id"))
+    $.ajax({
+
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+
+    .done(function(messages) {
+        console.log(messages)
+      messages.forEach(function( message ) {
+        var html =""
+        html = buildHTML(message);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'slow');
+      });
+
+    })
+    .fail(function() {
+
+    });
+  };
+    setInterval(reloadMessages, 5000);
 });
---
+++ app/views/api/messages/index.json.jbuilder
@@ -0,0 +1,8 @@
+json.array! @messages do |message|
+json.id        message.id
+json.content   message.content
+json.date      message.created_at.strftime("%Y/%m/%d %H:%M")
+json.user_name message.user.name
+json.image     message.image.url
+
+end
